3f27e1fbd7ff37b46ff2ba02610ca4dffca79cda
Derrick <derrick992110@gmail.com>
2024-08-28 10:22:36 +0800

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,14 +1,13 @@
-project "ImGui"
-	kind "StaticLib"
-	language "C++"
-    staticruntime "off"
+project "imgui"
+    kind "StaticLib"
+    language "C++"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    targetdir("bin/" ..outputdir.. "/%{prj.name}")
+    objdir("bin-int/" ..outputdir.. "/%{prj.name}")
 
-	files
-	{
-		"imconfig.h",
+    files
+    {
+        "imconfig.h",
 		"imgui.h",
 		"imgui.cpp",
 		"imgui_draw.cpp",
@@ -19,26 +18,12 @@ project "ImGui"
 		"imstb_textedit.h",
 		"imstb_truetype.h",
 		"imgui_demo.cpp"
-	}
+    }
 
-	filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++17"
+    filter "system:windows"
+    systemversion "latest"
+    cppdialect "C++20"
+    staticruntime "On"
 
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		cppdialect "C++17"
-
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
-
-    filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
-        symbols "off"
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
